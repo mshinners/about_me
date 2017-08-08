9bcc15541dc11f5ae40d0e4c751b8ed0515319ed
Michael Shinners <mishinners@icloud.com>
2017-08-08 15:36:15 -0700

    working on question structure.

---
+++ app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+//Question 1
+var questionOne = 'Have I ever lived in a foriegn country?';
+var country = prompt(questionOne);
+if (country === 'yes'.toLowerCase()) {
+  alert ('You are Correct!');
+} else {
+  alert('Wrong Answer!');
+}
+
+// alert =
