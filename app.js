9c36cf9ee8accbe29d3b44b8b99b8f77980b743f
Michael Shinners <mishinners@icloud.com>
2017-08-09 15:45:27 -0700

    working on Q6.

--- app.js
+++ app.js
@@ -1,46 +1,56 @@
 'use strict';
+//
+// var questionOne = 'Have I ever lived in a foriegn country?';
+// var country = prompt(questionOne).toLowerCase();
+// if (country === 'yes' || country === 'y') {
+//   alert ('You are Correct! I lived in the Middle East country of Bahrain.');
+// } else {
+//   alert('Wrong Answer! I lived in Bahrain for 4 years.');
+// }
+// console.log('Question 1:' + questionOne + '| Answer:' + country);
+//
+// var questionTwo = 'Have I ever walked out of a movie becasue it was so bad?';
+// var movie = prompt(questionTwo).toLowerCase();
+// if (movie === 'yes' || movie === 'y') {
+//   alert ('You are Correct! I\'m sorry to say that the movie was "Space Balls"');
+// } else {
+//   alert('Wrong Answer! I actually walked out of "Space Balls" the first time I saw it.');
+// }
+// console.log('Question 2:' + questionTwo + '| Answer:' + movie);
+//
+// var questionThree = 'Have I ever had a paranormal experience?';
+// var ghost = prompt(questionThree).toLowerCase();
+// if (ghost === 'no' || ghost === 'n') {
+//   alert ('You are Correct! While I have heard things I can\'t explain, I wouldn\'t classify that as paranormal.');
+// } else {
+//   alert('Wrong Answer! I have never seen things flying through the air.');
+// }
+// console.log('Question 3:' + questionThree + '| Answer:' + ghost);
+//
+// var questionFour = 'Have I ever had a surprise party thrown for me?';
+// var party = prompt(questionFour).toLowerCase();
+// if (party === 'yes' || party === 'y') {
+//   alert ('You are Correct! My wife put one together for my 35th(?) birthday.');
+// } else {
+//   alert('Wrong Answer! I had one with a small group of friends when I turned 35.');
+// }
+// console.log('Question 4:' + questionFour + '| Answer:' + party);
+//
+// var questionFive = 'Have I ever thrown up on a roller coaster?';
+// var coaster = prompt(questionFive).toLowerCase();
+// if (coaster === 'no' || coaster === 'n') {
+//   alert ('You are Correct! I\'ve remained vommit-free on all amusement rides.');
+// } else {
+//   alert('Wrong Answer! That\'s totally gross and I never!');
+// }
+// console.log('Question 5:' + questionFive + '| Answer:' + coaster);
 
-var questionOne = 'Have I ever lived in a foriegn country?';
-var country = prompt(questionOne).toLowerCase();
-if (country === 'yes' || country === 'y') {
-  alert ('You are Correct! I lived in the Middle East country of Bahrain.');
-} else {
-  alert('Wrong Answer! I lived in Bahrain for 4 years.');
+var questionSix = 'I\'m thinking of a number between 1 and 20. Can you guess it?';
+var answer = prompt(questionSix);
+if (answer == 17) {
+  alert ('Spot on! Great Job!');
+} else if (answer < 17) {
+  alert ('Too Low, try again.');
+} else if (answer > 17) {
+  alert ('Too high, try again.');
 }
-console.log('Question 1:' + questionOne + '| Answer:' + country);
-
-var questionTwo = 'Have I ever walked out of a movie becasue it was so bad?';
-var movie = prompt(questionTwo).toLowerCase();
-if (movie === 'yes' || movie === 'y') {
-  alert ('You are Correct! I\'m sorry to say that the movie was "Space Balls"');
-} else {
-  alert('Wrong Answer! I actually walked out of "Space Balls" the first time I saw it.');
-}
-console.log('Question 2:' + questionTwo + '| Answer:' + movie);
-
-var questionThree = 'Have I ever had a paranormal experience?';
-var ghost = prompt(questionThree).toLowerCase();
-if (ghost === 'no' || ghost === 'n') {
-  alert ('You are Correct! While I have heard things I can\'t explain, I wouldn\'t classify that as paranormal.');
-} else {
-  alert('Wrong Answer! I have never seen things flying through the air.');
-}
-console.log('Question 3:' + questionThree + '| Answer:' + ghost);
-
-var questionFour = 'Have I ever had a surprise party thrown for me?';
-var party = prompt(questionFour).toLowerCase();
-if (party === 'yes' || party === 'y') {
-  alert ('You are Correct! My wife put one together for my 35th(?) birthday.');
-} else {
-  alert('Wrong Answer! I had one with a small group of friends when I turned 35.');
-}
-console.log('Question 4:' + questionFour + '| Answer:' + party);
-
-var questionFive = 'Have I ever thrown up on a roller coaster?';
-var coaster = prompt(questionFive).toLowerCase();
-if (coaster === 'no' || coaster === 'n') {
-  alert ('You are Correct! I\'ve remained vommit-free on all amusement rides.');
-} else {
-  alert('Wrong Answer! That\'s totally gross and I never!');
-}
-console.log('Question 5:' + questionFive + '| Answer:' + coaster);
